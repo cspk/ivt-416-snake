378570f31ca0212b71e52b240ce56a3ab0f360fc
weewe3 <weewe3@github.com>
2022-05-05 23:32:44 +0500

    repo:change snake structure

--- main.c
+++ main.c
@@ -6,10 +6,8 @@
 #define SNAKE '#'
 #define RIP 'X'
 #define WALL 'W'
-#define GRID ' '
 
 #define INIT_SNAKE_LENGTH 5
-#define MAX_SNAKE_LENGTH 500
 #define SNAKE_INCREASE 3
 
 #define LEFT 0
@@ -24,25 +22,22 @@
 
 #define FRAME_SPEED 100
 
+typedef struct Snake
+{
+    int head_row, head_col;
+    int tail_row, tail_col;
+} Snake;
 
-typedef struct Coord {
-    int row, col;
-} Coord;
-
-typedef struct Stack {
-    Coord head, tail;
-} Stack;
-
-char **board;
-Stack *snake;
+Snake *body;
 
 int snake_draw_length = 0;
 int snake_length = INIT_SNAKE_LENGTH;
 
-int direction = LEFT;
+int direction;
 
-int game_over = 0;
+int food_row, food_col;
 
+bool game_over = false;
 
 void init_ncurses() {
     initscr();
@@ -66,173 +61,171 @@ void finish_ncurses() {
 }
 
 void draw_char(int row, int col, char ch) {
-    move(row, col);
-    addch(ch);
+    mvaddch(row, col, ch);
     refresh();
 }
 
-void draw_food() {
-    Coord rand_coord;
-    while (true) {
-        rand_coord.row = rand() % (LINES - 2);
-        rand_coord.col = rand() % (COLS - 2);
-        if (board[rand_coord.row][rand_coord.col] == GRID) break;
-    }
-    board[rand_coord.row][rand_coord.col] = FOOD;
-    draw_char(rand_coord.row, rand_coord.col, FOOD);
+void erase_char(int row, int col) {
+    mvaddch(row, col, ' ');
+    refresh();
 }
 
-void clear_board() {
-    for (int i = 0; i < LINES; i++)
-        for (int j = 0; j < COLS; j++)
-            if(i == 0 || i == LINES - 1 
-            || j == 0 || j == COLS - 1) {
-                board[i][j] = WALL;
-                draw_char(i, j, WALL);
-            }
-            else board[i][j] = GRID;
+char get_char(int row, int col) {
+    return mvinch(row, col);
 }
 
 void init_board() {
-    board = (char**) malloc(LINES * sizeof(char*));
-    if (!board) {
-        finish_ncurses();
+    for(int col = 0; col < COLS; col++) {
+        draw_char(0, col, WALL);
+        draw_char(LINES - 1, col, WALL);
     }
-    for (int i = 0; i < LINES; i++) {
-        board[i] = (char*) malloc(COLS * sizeof(char));
-        if (!board[i]) {
-            finish_ncurses();
-        }
+    for(int row = 0; row < LINES; row++) {
+        draw_char(row, 0, WALL);
+        draw_char(row, COLS - 1, WALL);
     }
-    clear_board();
+}
+
+void draw_food() {
+    food_row = 2 + rand() % (LINES - 1);
+    food_col = 2 + rand() % (COLS - 1);
+    draw_char(food_row, food_col, FOOD);
 }
 
 void init_snake() {
-    snake = (Stack*)malloc(LINES * COLS * sizeof(Stack));
+    body = malloc((LINES - 2) * (COLS - 1) * sizeof(Snake));
+    body[0].head_row = LINES / 2;
+    body[0].head_col = COLS / 2;
     
-    snake[0].head.row = LINES / 2 - 1;
-    snake[0].head.col = COLS / 2 - 1;
-    
-    draw_char(snake[0].head.row, snake[0].head.col, SNAKE);
-    
+    direction = rand() % 4;
+
+    draw_char(body[0].head_row, body[0].head_col, SNAKE);
+
     snake_draw_length++;
 }
 
 void change_tail() {
-   for(int i = snake_draw_length; i > 0; i--)
-        snake[i].head = snake[i - 1].tail = snake[i - 1].head;
+   for(int i = snake_draw_length; i > 0; i--) {
+       body[i].head_row = body[i - 1].head_row;
+       body[i].head_col = body[i - 1].head_col;
+       body[i - 1].tail_row = body[i - 1].head_row;
+       body[i - 1].tail_col = body[i - 1].head_col;
+   }
 }
 
 void change_head() {
     switch(direction) {
         case LEFT:
-            change_tail();
-            snake[0].head.col--;
+            body[0].head_col--;
             break;
             
         case RIGHT:
-            change_tail();
-            snake[0].head.col++;
+            body[0].head_col++;
             break;
             
         case UP:
-            change_tail();
-            snake[0].head.row--;
+            body[0].head_row--;
             break;
             
         case DOWN:
-            change_tail();
-            snake[0].head.row++;
+            body[0].head_row++;
             break;
+    }
+
+    if(get_char(body[0].head_row, body[0].head_col) != SNAKE) {
+        draw_char(body[0].head_row, body[0].head_col, SNAKE);
+    }
+    else {
+        game_over = true;
+        draw_char(body[0].head_row, body[0].head_col, RIP);
     }
 }
 
-void change_direction() {
-    int keypressed = wgetch(stdscr);
-    switch(keypressed) {
-        case ERR:
-            return;
-            
-        case KEY_ESC:
-            finish_ncurses();
-            break;
-        
-        case KEY_R_LOWER:
-            clear_board();
-            snake_length = INIT_SNAKE_LENGTH;
-            snake_draw_length = 1;
-            snake[0].head.row = LINES / 2;
-            snake[0].head.col = COLS / 2;
-            direction = rand() % 4;
-            game_over = 0;
-            clear();
-            refresh();
-            clear_board();
-            draw_char(snake[0].head.row, snake[0].head.col, SNAKE);
-            draw_food();
-            break;
-            
-        case KEY_R_UPPER:
-            clear_board();
-            snake_length = INIT_SNAKE_LENGTH;
-            snake_draw_length = 1;
-            snake[0].head.row = LINES / 2;
-            snake[0].head.col = COLS / 2;
-            direction = rand() % 4;
-            game_over = 0;
-            clear();
-            refresh();
-            clear_board();
-            draw_char(snake[0].head.row, snake[0].head.col, SNAKE);
-            draw_food();
-            break;
-        
-        case KEY_LEFT:
-            if(direction != RIGHT) direction = LEFT;
-            break;
-            
-        case KEY_RIGHT:
-            if(direction != LEFT) direction = RIGHT;
-            break;
-            
-        case KEY_UP:
-            if(direction != DOWN) direction = UP;
-            break;
-            
-        case KEY_DOWN:
-            if(direction != UP) direction = DOWN;
-            break;
-    }
-}
-
-int verify_head() {
-    if (snake[0].head.row < 1 || snake[0].head.col < 1
-    || snake[0].head.row >= LINES - 1 || snake[0].head.col >= COLS - 1
-    || board[snake[0].head.row][snake[0].head.col] == '#') {
-        draw_char(snake[1].head.row, snake[1].head.col, RIP);
+int verify_wall() {
+    if (body[0].head_row == 0 || body[0].head_col == 0
+    || body[0].head_row == LINES - 1 || body[0].head_col == COLS - 1) {
+        draw_char(body[0].head_row, body[0].head_col, RIP);
         return 1;
     }
     return 0;
 }
 
 void snake_logic() {
+    change_tail();
     change_head();
-    if (verify_head()) {
-        direction = FREEZE;
-        game_over = 1;
+
+    if (verify_wall()) {
+        game_over = true;
+    }
+
+    if(game_over) {
         return;
     }
-    if (board[snake[0].head.row][snake[0].head.col] == FOOD) {
+
+    if (body[0].head_row == food_row && body[0].head_col == food_col) {
         snake_length += SNAKE_INCREASE;
         draw_food();
     }
-    board[snake[0].head.row][snake[0].head.col] = SNAKE;
-    draw_char(snake[0].head.row, snake[0].head.col, SNAKE);
     
-    if(snake_draw_length <= snake_length) snake_draw_length++;
+    if(snake_draw_length <= snake_length) {
+        snake_draw_length++;
+    }
     else {
-        board[snake[snake_draw_length - 1].tail.row][snake[snake_draw_length - 1].tail.col] = GRID;
-        draw_char(snake[snake_draw_length - 1].tail.row, snake[snake_draw_length - 1].tail.col, GRID);
+        erase_char(body[snake_draw_length - 1].tail_row, body[snake_draw_length - 1].tail_col);
+    }
+}
+
+void restart_game() {
+    snake_length = INIT_SNAKE_LENGTH;
+    snake_draw_length = 1;
+    body[0].head_row = LINES / 2;
+    body[0].head_col = COLS / 2;
+    direction = rand() % 4;
+    game_over = false;
+    clear();
+    init_board();
+    refresh();
+    draw_char(body[0].head_row, body[0].head_col, SNAKE);
+    draw_food();
+}
+
+void change_direction() {
+    int keypressed = getch();
+    switch (keypressed) {
+        case KEY_LEFT:
+            if(direction != RIGHT) {
+               direction = LEFT; 
+            }
+            break;
+        
+        case KEY_RIGHT:
+            if(direction != LEFT) {
+               direction = RIGHT; 
+            }
+            break;
+        
+        case KEY_UP:
+            if(direction != DOWN) {
+               direction = UP; 
+            }
+            break;
+        
+        case KEY_DOWN:
+            if(direction != UP) {
+               direction = DOWN; 
+            }
+            break;
+
+        case KEY_ESC:
+            finish_ncurses();
+            break;
+
+        case KEY_R_LOWER:
+            restart_game();
+            break;
+        
+        case KEY_R_UPPER:
+            restart_game();
+            break;
     }
 }
 
@@ -240,15 +233,16 @@ int main() {
     srand((unsigned) time(NULL));
     init_ncurses();
     init_board();
-    init_snake();
     draw_food();
+    init_snake();
     while(true) {
         change_direction();
-        napms(100);
-        if(direction != FREEZE && !game_over) snake_logic();    
+        napms(FRAME_SPEED);
+        if(!game_over) {
+            snake_logic();
+        }
     }
     finish_ncurses();
     
     return 0;
 }
-
